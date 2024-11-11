12dbc99070fc2977adc057bdf29fa51c77e0baab
pandalonium <butchej@stpaulsschool.org.uk>
2024-11-11 15:56:19 +0000

    Added some more motion testing

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+var devicex = 0;
+var devicey = 0;
+var velx = 0;
+var vely = 0;
+
 window.onload = () => {
     window.addEventListener("devicemotion", (e) => {
-        document.write(String(e.acceleration.x));
+        let dt = e.interval / 1000;
+
+        velx += dt*e.acceleration.x/2;
+        devicex += dt*velx;
+        velx += dt*e.acceleration.x/2;
+
+        vely += dt*e.acceleration.y/2;
+        devicey += dt*vely;
+        vely += dt*e.acceleration.y/2;
+        console.log(`accel: ${e.acceleration.x} ${e.acceleration.y}<br>vel: ${velx} ${vely}<br>pos: ${devicex} ${devicey}`)
+
+        document.body.innerHTML= (`accel: ${e.acceleration.x} ${e.acceleration.y}<br>vel: ${velx} ${vely}<br>pos: ${devicex} ${devicey}`)
     })
 }
