417308820aa99e5ce381471630e9073fa8058ee0
pandalonium <butchej@stpaulsschool.org.uk>
2024-11-11 16:52:28 +0000

    Made it compatible with ios

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 var devicex = 0;
 var devicey = 0;
 var velx = 0;
-var vely = 0;
+var rotation = 0;
+var angularvel = 0;
 
 window.onload = () => {
-    window.addEventListener("devicemotion", (e) => {
-        let dt = e.interval / 1000;
-
-        velx += dt*e.acceleration.x/2;
-        devicex += dt*velx;
-        velx += dt*e.acceleration.x/2;
-
-        vely += dt*e.acceleration.y/2;
-        devicey += dt*vely;
-        vely += dt*e.acceleration.y/2;
-        console.log(`accel: ${e.acceleration.x} ${e.acceleration.y}<br>vel: ${velx} ${vely}<br>pos: ${devicex} ${devicey}`)
-
-        document.body.innerHTML= (`accel: ${e.acceleration.x} ${e.acceleration.y}<br>vel: ${velx} ${vely}<br>pos: ${devicex} ${devicey}`)
+    DeviceMotionEvent.requestPermission().then(() => {
+        window.addEventListener("devicemotion", (e) => {
+            let dt = e.interval / 1000;
+    
+            velx += dt*e.acceleration.x/2;
+            devicex += dt*velx;
+            velx += dt*e.acceleration.x/2;
+    
+            angularvel += dt*e.rotationRate.alpha/2;
+            rotation += dt*angularvel;
+            angularvel += dt*e.rotationRate.alpha/2;
+            document.body.innerHTML = `${rotation} ${angularvel} ${e.rotationRate.alpha}`
+            
+    
+        })
     })
+    
 }
